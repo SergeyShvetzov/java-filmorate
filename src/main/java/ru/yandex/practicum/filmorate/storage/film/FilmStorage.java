5b67cff2996391f6b52d0824e0a13ad18a658add
Sergey Shvetsov <shvetsov.sergey98@yandex.ru>
2023-11-14 11:07:36 +0300

    Добавление рейтинга фильмов и возможность добавления пользователей в друзья

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -7,10 +7,16 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.Collection;
 
 public interface FilmStorage {
+
     Collection<Film> getFilm(); // отправляет список всех фильмов
+
     Film findFilmById(@PathVariable long filmId); // отправляет фильм по Id
+
     Film postFilm(@RequestBody Film film); // добавляет фильм
+
     Film updateFilm(@RequestBody Film film); // обновляет данные фильма
+
     String deleteAllFilms(); // удаляет все фильмы
+
     String deleteFilmById(@PathVariable long filmId); // удаляет фильм по Id
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -7,13 +7,20 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.Collection;
 
 public interface UserStorage {
+
     Collection<User> getUsers(); // отправляет список всех пользователей
 
     User findUserById(@PathVariable long userId); // отправляет пользователя по id
+
     User postUser(@RequestBody User user); // добавляет пользователя
+
     User updateUser(@RequestBody User user); // обновляет данные пользователя
+
     String deleteAllUsers(); // удалет список всех пользователей
+
     String deleteUserById(@PathVariable long userId);// удаляет пользователя по id
+
     Collection<User> getUserFriends(long userId); //  получение списка друзей пользователя
+
     Collection<User> getListMutualFriends(long id, long otherId); // получение списка общего списка друзей другого пользователя
 }
