f16d08e710b85146ea005b242049af70a1974385
Sergey Shvetsov <shvetsov.sergey98@yandex.ru>
2023-11-15 08:37:57 +0300

    Исправление зависимости

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,10 +2,8 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exceptions.IncorrectCountException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.film.FilmService;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import java.util.Collection;
 
@@ -13,49 +11,47 @@ import java.util.Collection;
 @RequestMapping({"/films"})
 public class FilmController {
 
-    private final FilmStorage filmStorage;
     private final FilmService filmService;
 
     @Autowired
-    public FilmController(FilmStorage filmStorage, FilmService filmService) {
-        this.filmStorage = filmStorage;
+    public FilmController(FilmService filmService) {
         this.filmService = filmService;
     }
 
     // получить список фильмов
     @GetMapping
     public Collection<Film> getFilm() {
-        return filmStorage.getFilm();
+        return filmService.getFilm();
     }
 
     // получение фильма по id
     @GetMapping("/{filmId}")
     public Film findFilmById(@PathVariable long filmId) {
-        return filmStorage.findFilmById(filmId);
+        return filmService.findFilmById(filmId);
     }
 
     // добавление фильма
     @PostMapping
     public Film postFilm(@RequestBody Film film) {
-        return filmStorage.postFilm(film);
+        return filmService.postFilm(film);
     }
 
     // обновление данных фильма
     @PutMapping
     public Film updateFilm(@RequestBody Film film) {
-        return filmStorage.updateFilm(film);
+        return filmService.updateFilm(film);
     }
 
     // удаление всех фильмов
     @DeleteMapping
     public String deleteFilm() {
-        return filmStorage.deleteAllFilms();
+        return filmService.deleteAllFilms();
     }
 
     // удаление фильма по id
     @DeleteMapping("/{filmId}")
     public String deleteFilmById(@PathVariable long filmId) {
-        return filmStorage.deleteFilmById(filmId);
+        return filmService.deleteFilmById(filmId);
     }
 
     // PUT /films/{id}/like/{userId} — пользователь ставит лайк фильму.
@@ -72,14 +68,7 @@ public class FilmController {
 
     // GET /films/popular?count={count} — возвращает список из первых count фильмов по количеству лайков.
     @GetMapping("/popular")
-    public Collection<Film> getFilmCountLikes(@RequestParam(defaultValue = "10", required = false) Integer count) {
-        if (count == null) {
-            throw new IncorrectCountException("Параметр count равен null.");
-        }
-        if (count <= 0) {
-            throw new IncorrectCountException("Параметр count имеет отрицательное значение.");
-        }
-
+    public Collection<Film> getFilmCountLikes(@RequestParam(defaultValue = "10", required = false) Integer count)   {
         return filmService.getFilmCountLikes(count);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.user.UserService;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.Collection;
 
@@ -12,49 +11,47 @@ import java.util.Collection;
 @RequestMapping("/users")
 public class UserController {
 
-    private final UserStorage userStorage;
     private final UserService userService;
 
     @Autowired
-    public UserController(UserStorage userStorage, UserService userService) {
-        this.userStorage = userStorage;
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
     // получение списка всех пользователей
     @GetMapping
     public Collection<User> getUsers() {
-        return userStorage.getUsers();
+        return userService.getUsers();
     }
 
     // получение пользователя по id
     @GetMapping("/{userId}")
     public User findUserById(@PathVariable long userId) {
-        return userStorage.findUserById(userId);
+        return userService.findUserById(userId);
     }
 
     // добавление пользователя
     @PostMapping
     public User postUser(@RequestBody User user) {
-        return userStorage.postUser(user);
+        return userService.postUser(user);
     }
 
     // обновление данных пользователя
     @PutMapping
     public User updateUser(@RequestBody User user) {
-        return userStorage.updateUser(user);
+        return userService.updateUser(user);
     }
 
     // удалить всех пользователей
     @DeleteMapping
     public String deleteAllUsers() {
-        return userStorage.deleteAllUsers();
+        return userService.deleteAllUsers();
     }
 
     // удалить пользователя по id
     @DeleteMapping("/user/{userId}")
     public String deleteUserById(@PathVariable long userId) {
-        return userStorage.deleteUserById(userId);
+        return userService.deleteUserById(userId);
     }
 
     // PUT /users/{id}/friends/{friendId} — добавление в друзья.
