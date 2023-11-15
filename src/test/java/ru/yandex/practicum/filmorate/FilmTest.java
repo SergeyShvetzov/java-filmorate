9df651b72df3dd79675c18c7fe0bdc3e536c5c86
Sergey Shvetsov <shvetsov.sergey98@yandex.ru>
2023-11-15 08:37:07 +0300

    Реализация тестов для классов FilmService, InMemoryFilmStorage и UserService, InMemoryUserStorage

---
+++ src/test/java/ru/yandex/practicum/filmorate/FilmTest.java
@@ -0,0 +1,201 @@
+
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exceptions.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.IncorrectCountException;
+import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.film.FilmService;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+public class FilmTest <T extends FilmStorage> {
+
+    private FilmService filmService;
+
+    @BeforeEach
+    public void setUp() {
+        InMemoryFilmStorage filmStorage = new InMemoryFilmStorage();
+        filmService = new FilmService(filmStorage);
+    }
+
+    @Test
+    public void filmReceiptCheck() {
+        Film film = new Film(1, "Титаник", "Описание", LocalDate.now(), 60, null, 0);
+        filmService.postFilm(film);
+        Collection<Film> filmList = filmService.getFilm();
+        Assertions.assertNotNull(filmList);
+        Assertions.assertEquals(1, filmList.size());
+    }
+
+    @Test
+    public void filmUpdateAndCheckResult() {
+        Film film = new Film(1, "Титаник", "Описание", LocalDate.now(), 60, null, 0);
+        filmService.postFilm(film);
+        Film film1 = new Film(1, "Интерстеллар", "Описание", LocalDate.now(), 90, null, 0);
+        filmService.updateFilm(film1);
+        long filmId = film.getId();
+        long film1Id = film1.getId();
+        Assertions.assertEquals(filmId, film1Id);
+        String filmName = film.getName();
+        String film1Name = film1.getName();
+        Assertions.assertNotEquals(film, film1);
+        Assertions.assertNotEquals(filmName, film1Name);
+    }
+
+    @Test
+    public void filmEmptyName() {
+        Film film = new Film(1, "", "Описание", LocalDate.now(), 60, null, 0);
+        Assertions.assertThrows(ValidationException.class, () -> {
+            filmService.postFilm(film);
+        });
+        Film film1 = new Film(1, " ", "Описание", LocalDate.now(), 60, null, 0);
+        Assertions.assertThrows(ValidationException.class, () -> {
+            filmService.postFilm(film1);
+        });
+    }
+
+    @Test
+    public void checkingTheMaximumLengthOf200CharactersForAMovieDescription() {
+        String symbol201 = "";
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < 104; ++i) {
+            symbol201 = String.valueOf(i);
+            result.append(symbol201);
+        }
+
+        Film film = new Film(1, "Интерстеллар", result.toString(), LocalDate.now(),
+                60, null, 0);
+        Assertions.assertThrows(ValidationException.class, () -> {
+            filmService.postFilm(film);
+        });
+    }
+
+    @Test
+    public void releaseDateCheckMustNotBeEarlierThan28December1895year() {
+        Film film = new Film(1, "Интерстеллар", "Описание",
+                LocalDate.of(1895, 12, 27), 60, null, 0);
+        Assertions.assertThrows(ValidationException.class, () -> {
+            filmService.postFilm(film);
+        });
+    }
+
+    @Test
+    public void checkingMovieDurationForANegativeValue() {
+        Film film = new Film(1, "Интерстеллар", "Описание", LocalDate.now(),
+                -1, null, 0);
+        Assertions.assertThrows(ValidationException.class, () -> {
+            filmService.postFilm(film);
+        });
+    }
+
+    @Test
+    public void getFilmByIdMustReturn1Film() {
+        Film film = new Film(1, "Интерстеллар", "Описание", LocalDate.now(),
+                120, null, 0);
+        filmService.postFilm(film);
+        Film savedFilm = filmService.findFilmById(film.getId());
+        Assertions.assertNotNull(savedFilm, "Фильм не вернулся");
+        Assertions.assertEquals(film, savedFilm, "Фильмы не равны");
+    }
+
+    @Test
+    public void checkingToReturnFilmWithAnIncorrectIdShouldGiveAnError() {
+        Film film = new Film(1, "Интерстеллар", "Описание", LocalDate.now(),
+                120, null, 0);
+        filmService.postFilm(film);
+        Film film2 = new Film(0, "Интерстеллар 2", "Описание", LocalDate.now(),
+                120, null, 0);
+        Assertions.assertThrows(ValidationException.class, () -> {
+            filmService.updateFilm(film2);
+        });
+    }
+
+    @Test
+    public void deletingAllFilmsTheListMustBeEmpty() {
+        Film film = new Film(1, "Интерстеллар", "Описание", LocalDate.now(),
+                120, null, 0);
+        filmService.postFilm(film);
+        Film film2 = new Film(2, "Интерстеллар 2", "Описание", LocalDate.now(),
+                120, null, 0);
+        filmService.postFilm(film2);
+        Collection<Film> filmList = filmService.getFilm();
+        filmService.deleteAllFilms();
+        Assertions.assertEquals(0, filmList.size(), "Список после удаления всех фильмов не пуст");
+    }
+
+    @Test
+    public void deleteFilmByIdTheListMustBeLessThanOneLong() {
+        Film film = new Film(1, "Интерстеллар", "Описание", LocalDate.now(),
+                120, null, 0);
+        filmService.postFilm(film);
+        Film film2 = new Film(2, "Интерстеллар 2", "Описание", LocalDate.now(),
+                120, null, 0);
+        filmService.postFilm(film2);
+        Assertions.assertThrows(FilmNotFoundException.class, () -> {
+            filmService.deleteFilmById(3);
+        });
+        Collection<Film> filmList = filmService.getFilm();
+        Assertions.assertEquals(2, filmList.size(), "Список не равен 2");
+        filmService.deleteFilmById(2);
+        Assertions.assertEquals(1, filmList.size(), "Фильм не был удален, длина списка 2");
+    }
+
+    @Test
+    public void addingALikeToAFilm() {
+        Film film = new Film(1, "Интерстеллар", "Описание", LocalDate.now(),
+                120, null, 0);
+        filmService.postFilm(film);
+        filmService.putLikeFilm(1, 1);
+        Assertions.assertThrows(UserNotFoundException.class, () -> {
+            filmService.putLikeFilm(1, -1);
+        });
+        Integer likesCount = film.getLikesCount();
+        Assertions.assertEquals(1, likesCount, "Количество лайков не соответствует");
+    }
+
+    @Test
+    public void removingAFIlmLike() {
+        Film film = new Film(1, "Интерстеллар", "Описание", LocalDate.now(),
+                120, null, 0);
+        filmService.postFilm(film);
+        filmService.putLikeFilm(1, 1);
+        Integer savedlikesCount = film.getLikesCount();
+        Assertions.assertEquals(1, savedlikesCount, "Количество лайков не соответствует");
+        Assertions.assertThrows(UserNotFoundException.class, () -> {
+            filmService.deleteLike(1, -1);
+        });
+        filmService.deleteLike(1, 1);
+        Integer likesCount = film.getLikesCount();
+        Assertions.assertEquals(0, likesCount, "Лайк не удален");
+    }
+
+    @Test
+    public void gettingListOfFilmsByNumberOfLikes() {
+        Film film = new Film(1, "Интерстеллар", "Описание", LocalDate.now(),
+                120, null, 0);
+        filmService.postFilm(film);
+        Film film2 = new Film(2, "Интерстеллар 2", "Описание", LocalDate.now(),
+                120, null, 0);
+        filmService.postFilm(film2);
+        filmService.putLikeFilm(1, 1);
+        filmService.putLikeFilm(2, 2);
+        Assertions.assertThrows(IncorrectCountException.class, () -> {
+            filmService.getFilmCountLikes(null);
+        });
+        Assertions.assertThrows(IncorrectCountException.class, () -> {
+            filmService.getFilmCountLikes(-1);
+        });
+        Collection<Film> listLikesFilm = filmService.getFilmCountLikes(2);
+        Assertions.assertEquals(2, listLikesFilm.size(), "Количество списка не соответствует.");
+    }
+}
+
---
+++ src/test/java/ru/yandex/practicum/filmorate/UserTest.java
@@ -0,0 +1,210 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.user.UserService;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Set;
+
+public class UserTest <T extends UserStorage> {
+
+    private UserService userService;
+
+    @BeforeEach
+    public void setUp() {
+        InMemoryUserStorage userStorage = new InMemoryUserStorage();
+        userService = new UserService(userStorage);
+    }
+
+    @Test
+    public void userReceiptCheck() {
+        User user = new User(1, "email@", "login", "name", LocalDate.now(), null);
+        userService.postUser(user);
+        Collection<User> filmList = userService.getUsers();
+        Assertions.assertNotNull(filmList);
+        Assertions.assertEquals(1, filmList.size());
+    }
+
+    @Test
+    public void gettingUserByIdShouldReturnOne() {
+        User user = new User(1, "email@", "login", "name", LocalDate.now(), null);
+        userService.postUser(user);
+        User savedUser = userService.findUserById(1);
+        Assertions.assertNotNull(savedUser, "Пользователь не возвращен.");
+        Assertions.assertEquals(user, savedUser, "Пользователи не равны.");
+    }
+
+    @Test
+    public void userUpdateAndCheckResult() {
+        User user = new User(1, "email@", "login", "name", LocalDate.now(), null);
+        userService.postUser(user);
+        User user1 = new User(1, "email@", "login", "Sergey", LocalDate.now(), null);
+        userService.updateUser(user1);
+        User user2 = new User(3, "email@", "login", "Sergey", LocalDate.now(), null);
+        Assertions.assertThrows(UserNotFoundException.class, () -> {
+            userService.updateUser(user2);
+        });
+        long userId = user.getId();
+        long user1Id = user1.getId();
+        Assertions.assertEquals(userId, user1Id);
+        String filmName = user.getName();
+        String film1Name = user1.getName();
+        Assertions.assertNotEquals(user, user1);
+        Assertions.assertNotEquals(filmName, film1Name);
+    }
+
+    @Test
+    public void deletingAllUsersShouldReturnAnEmptyList() {
+        User user = new User(1, "email@", "login", "name", LocalDate.now(), null);
+        userService.postUser(user);
+        User user2 = new User(2, "email.2@", "login 2", "name 2", LocalDate.now(), null);
+        userService.postUser(user2);
+        Collection<User> usersList = userService.getUsers();
+        Assertions.assertEquals(2, usersList.size(), "Список не равен количеству пользователей");
+        userService.deleteAllUsers();
+        Assertions.assertEquals(0, usersList.size(), "Пользователи не удалены");
+    }
+
+    @Test
+    public void deleteUserByIdTheListMustBeLessThanOneLong() {
+        User user = new User(1, "email@", "login", "name", LocalDate.now(), null);
+        userService.postUser(user);
+        User user2 = new User(2, "email.2@", "login 2", "name 2", LocalDate.now(), null);
+        userService.postUser(user2);
+        Collection<User> usersList = userService.getUsers();
+        Assertions.assertEquals(2, usersList.size(), "Список не равен количеству пользователей.");
+        userService.deleteUserById(1);
+        Assertions.assertThrows(UserNotFoundException.class, () -> {
+            userService.deleteUserById(3);
+        });
+        Assertions.assertEquals(1, usersList.size(), "Пользователь не удален.");
+    }
+
+    @Test
+    public void addingUserTwoAsAFriendToUserOne() {
+        User user = new User(1, "email@", "login", "name", LocalDate.now(), null);
+        userService.postUser(user);
+        User user2 = new User(2, "email.2@", "login 2", "name 2", LocalDate.now(), null);
+        userService.postUser(user2);
+        userService.addInFriend(1, 2);
+        Set<Long> friendsUser1 = user.getFriends();
+        Assertions.assertEquals(1, friendsUser1.size(), "Количество друзей не совпадает.");
+        Set<Long> friendsUser2 = user2.getFriends();
+        Assertions.assertEquals(1, friendsUser2.size(), "Пользователь 1 не добавился " +
+                "в друзья пользователю 2");
+    }
+
+    @Test
+    public void unfriendingAnotherUser() {
+        User user = new User(1, "email@", "login", "name", LocalDate.now(), null);
+        userService.postUser(user);
+        User user2 = new User(2, "email.2@", "login 2", "name 2", LocalDate.now(), null);
+        userService.postUser(user2);
+        userService.addInFriend(1, 2);
+        Set<Long> friendsUser1 = user.getFriends();
+        Assertions.assertEquals(1, friendsUser1.size(), "Количество друзей не совпадает.");
+        userService.removeFromFriends(1, 2);
+        Assertions.assertEquals(0, friendsUser1.size(), "Друг не был удален.");
+        Set<Long> friendsUser2 = user2.getFriends();
+        Assertions.assertEquals(0, friendsUser2.size(), "Пользователь 1 автоматически " +
+                "не удалился из друзей пользователя 2");
+    }
+
+    @Test
+    public void GettingListOfAllTheUsersFriends() {
+        User user = new User(1, "email@", "login", "name", LocalDate.now(), null);
+        userService.postUser(user);
+        User user2 = new User(2, "email.2@", "login 2", "name 2", LocalDate.now(), null);
+        userService.postUser(user2);
+        userService.addInFriend(1, 2);
+        Collection<User> userFriends = userService.getUserFriends(1);
+        Assertions.assertThrows(UserNotFoundException.class, () -> {
+            userService.getUserFriends(3);
+        });
+        Assertions.assertEquals(1, userFriends.size(), "Количество друзей не совпадает.");
+    }
+
+    @Test
+    public void GettingListOfMutualFriendsOfAnotherUser() {
+        User user = new User(1, "email@", "login", "name", LocalDate.now(), null);
+        userService.postUser(user);
+        User user2 = new User(2, "email.2@", "login 2", "name 2", LocalDate.now(), null);
+        userService.postUser(user2);
+        User user3 = new User(3, "email.3@", "login 3", "name 3", LocalDate.now(), null);
+        userService.postUser(user3);
+        User user4 = new User(4, "email.4@", "login 4", "name 4", LocalDate.now(), null);
+        userService.postUser(user4);
+        userService.addInFriend(1, 2);
+        userService.addInFriend(1, 3);
+        userService.addInFriend(2, 2);
+        userService.addInFriend(2, 3);
+        userService.addInFriend(2, 4);
+        Collection<User> userFriends = userService.getListMutualFriends(1, 2);
+        Assertions.assertThrows(UserNotFoundException.class, () -> {
+            userService.getListMutualFriends(1, 5);
+        });
+        Assertions.assertThrows(UserNotFoundException.class, () -> {
+            userService.getListMutualFriends(5, 1);
+        });
+        Assertions.assertEquals(2, userFriends.size(), "Количество общих друзей не совпадает.");
+    }
+
+    @Test
+    public void userEmptyEmail() {
+        User user = new User(1, "", "login", "name", LocalDate.now(), null);
+        Assertions.assertThrows(ValidationException.class, () -> {
+            userService.postUser(user);
+        });
+        User user1 = new User(1, "           ", "login", "name", LocalDate.now(), null);
+        Assertions.assertThrows(ValidationException.class, () -> {
+            userService.postUser(user1);
+        });
+    }
+
+    @Test
+    public void checkingEmailForSymbol() {
+        User user = new User(1, "email", "login", "name",
+                LocalDate.now().plusDays(1L), null);
+        Assertions.assertThrows(ValidationException.class, () -> {
+            userService.postUser(user);
+        });
+    }
+
+    @Test
+    public void loginMustNotBeEmptyOrContainSpaces() {
+        User user = new User(1, "email@", "", "name",
+                LocalDate.now().plusDays(1L), null);
+        Assertions.assertThrows(ValidationException.class, () -> {
+            userService.postUser(user);
+        });
+        User user1 = new User(1, "email@", "    ", "name",
+                LocalDate.now().plusDays(1L), null);
+        Assertions.assertThrows(ValidationException.class, () -> {
+            userService.postUser(user1);
+        });
+    }
+
+    @Test
+    public void ifThNameIsEmptyThenItShouldBeTheSameAsTheLogin() {
+        User user = new User(1, "email@", "login", "", LocalDate.now(), null);
+        userService.postUser(user);
+        Assertions.assertEquals(user.getLogin(), user.getName());
+    }
+
+    @Test
+    public void checkDateOfBirthForFutureTense() {
+        User user = new User(1, "email@", "login", "name",
+                LocalDate.now().plusDays(1L), null);
+        Assertions.assertThrows(ValidationException.class, () -> {
+            userService.postUser(user);
+        });
+    }
+}
